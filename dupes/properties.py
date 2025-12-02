d8bdc7af1798493568e620e795a9d6f41eb6725c
Fadri Pestalozzi <fadri.pestalozzi@gmail.com>
2025-12-02 12:44:31 +0100

    feature: add encoding of the cetegories

--- dupes/properties.py
+++ dupes/properties.py
@@ -1,5 +1,5 @@
 import pandas as pd
-from sklearn.preprocessing import LabelEncoder
+from sklearn.preprocessing import MultiLabelBinarizer
 from unidecode import unidecode
 
 def clean_categories(dataframe):
@@ -19,5 +19,19 @@ def clean_categories(dataframe):
     dupes_df["propiedad"] = dupes_df["propiedad"].map(lambda x: [unidecode(v) for v in x])
     
     dupes_df["propiedad"] = dupes_df["propiedad"].map(lambda x: [v.replace("-"," ")for v in x])
-        
+    
+    dupes_df.reset_index(drop=True, inplace=True)
+       
     return dupes_df
+
+
+def encode_properties(dataframe):
+    
+    mlb = MultiLabelBinarizer()
+    
+    mlb_df = pd.DataFrame(mlb.fit_transform(dataframe["propiedad"]),
+                 columns=mlb.classes_,
+                 index=dataframe.index
+                 )
+    
+    return pd.concat([dataframe.drop(columns=["propiedad"]),mlb_df], axis=1)
