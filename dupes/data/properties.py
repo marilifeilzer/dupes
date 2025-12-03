c9c334b0918673fcc9c86717ef4bc7af3782040b
Francisco Requena <requena.francisco@protonmail.com>
2025-12-03 17:38:54 +0100

    feat: add ohe for color type and clean price

--- dupes/data/properties.py
+++ dupes/data/properties.py
@@ -60,3 +60,85 @@ def price_and_vol_clean(data):
 
     #Drop original columns
     data = data.drop(columns=["price", "volume"])
+    return data
+
+def encode_hair_colors(data):
+    #Fixed set of categories for hair color
+    HAIR_COLORS = [
+        "Todos los colores de cabello",
+        "Cabello rubio",
+        "Cabello Rubio Platino",
+        "Cabello Blanco-Gris",
+        "Cabello gris",
+        "Cabello castaño",
+    ]
+
+    #Impute NaN in the original column with the mode
+    df["color_de_cabello"] = df["color_de_cabello"].fillna("Todos los colores de cabello")
+
+    #Convert strings into lists
+    def split_colors(x: str):
+        return [p.strip() for p in str(x).split(",") if p.strip()]
+
+    df["color_list"] = df["color_de_cabello"].apply(split_colors)
+
+    #One-hot encode from the lists
+    dummies = (
+        df["color_list"]
+        .explode()
+        .str.get_dummies()
+        .groupby(level=0)
+        .sum()
+    )
+
+    dummies = dummies[HAIR_COLORS]
+    df = pd.concat([df, dummies], axis=1)
+    df = df.drop(columns=["color_list", "color_de_cabello"])
+    return df
+
+def encode_hair_type(data):
+    HAIR_TYPES = [
+        "Todo tipo de cabello",
+        "Cabellos teñidos",
+        "Cabello fino",
+        "Cabello estresado",
+        "Cabello seco",
+        "Cabello quebradizo",
+        "Cabello rizado",
+        "Cabello normal",
+        "Cabello con volumen",
+        "Cabello apagado",
+        "Cabello ondulado",
+        "Cabello grueso",
+        "Cabello graso",
+        "Cuero cabelludo sensible",
+        "Cuero cabelludo",
+        "Cabello rebelde",
+        "Cabello dañado por el sol",
+        "Cabello liso",
+        "Cabello dañado",
+    ]
+
+    #Impute NaN in the original column with the mode
+    df["tipo_de_cabello"] = df["tipo_de_cabello"].fillna("Todo tipo de cabello")
+
+    #Split values into lists
+    def split_hair_types(x: str):
+        return [p.strip() for p in str(x).split(",") if p.strip()]
+
+    df["tipo_list"] = df["tipo_de_cabello"].apply(split_hair_types)
+
+    #One-hot encode from the lists
+    dummies = (
+        df["tipo_list"]
+        .explode()
+        .str.get_dummies()
+        .groupby(level=0)
+        .sum()
+    )
+
+    dummies = dummies[HAIR_TYPES]
+    df = pd.concat([df, dummies], axis=1)
+    df = df.drop(columns=["tipo_list", "tipo_de_cabello"])
+    
+    return df
