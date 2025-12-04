e4617ba01752b03936b6d3dda8fa049fd2a14ce9
jonathanmorenoram <jonathanmorenoram@gmail.com>
2025-12-04 17:07:16 +0100

    feature: add the UI  (#11)
    * chore: added notebooks to git ignore
    
    * chore: add setup
    
    * chore: add the Dockerfile
    
    * chore: add the gcp file
    
    * feature: add the base UI
    
    ---------

---
+++ dupes/api/fast.py
@@ -0,0 +1,21 @@
+from fastapi import FastAPI
+from dupes.logic import predict_shampoo
+
+app = FastAPI()
+
+@app.get("/")
+def index():
+    return {"working": True}
+
+@app.get("/recomend")
+def get_recomendation(shampoo: str, description: str):
+    
+    if shampoo:
+        
+        recomendation = predict_shampoo(shampoo)
+    else:
+        
+        recomendation = predict_shampoo(description)
+
+    
+    return {"recomendation": recomendation}
---
+++ dupes/logic.py
@@ -0,0 +1,3 @@
+def predict_shampoo(shampoo:str):
+    
+    return "One / Two / Three / Four / Five"
