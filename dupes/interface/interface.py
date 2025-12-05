60c062eba7ae1e1e79dafa73c0daadafcc8d79e9
Fadri Pestalozzi <fadri.pestalozzi@gmail.com>
2025-12-05 16:34:33 +0100

    feature: link the api with the logic

--- dupes/interface/interface.py
+++ dupes/interface/interface.py
@@ -8,34 +8,26 @@ st.markdown("""
             let us help you find the cheapest shampoo in the market.
             """)
 
-# User Input 
-    
-shampoo_brand_text = st.text_input('You can either tell us the name of the shampoo you need:', placeholder="L'Oreal")
+# User Input
 
-nlp_text = st.text_input("Or you can give me a description for the kind of shampoo you are looking for",placeholder=" I need a moisturuzing shampoo")
+#shampoo_brand_text = st.text_input('You can either tell us the name of the shampoo you need:', placeholder="L'Oreal")
 
-    
+nlp_text = st.text_input("Tell me what kind of shampoo you are looking for:",\
+    placeholder=" I need a moisturuzing shampoo for...")
 
-params = dict(
-    shampoo=shampoo_brand_text,
-    description=nlp_text
-)
 
-# TODO: Change the api URL to google after the test in local
+if nlp_text:
+    params = dict(description=nlp_text)
 
-dupes_web_api = "http://127.0.0.1:8000/recomend"
-response = requests.get(dupes_web_api,params=params)
+    # TODO: Change the api URL to google after the test in local
 
-prediction = response.json()
+    dupes_web_api = "http://127.0.0.1:8000/recomend"
+    response = requests.get(dupes_web_api,params=params)
 
-predicted_shampoo = prediction["recomendation"]
+    predictions = response.json()
 
-# Check for a valid input to output the results
+    for prediction in predictions:
 
-if shampoo_brand_text:
-    
-    st.header(f"The most similars shampoos to {shampoo_brand_text} we could find are: {predicted_shampoo}")
-
-else:
-    
-    st.header(f"We recomend you the following shampoo: {predicted_shampoo}")
+        st.text(f"Name of the shampoo {list(prediction["product_name"].values())[0]}")
+        st.text(f"Price of the shampoo {list(prediction["price_eur"].values())[0]}")
+        st.text(f"Description of the shampoo {list(prediction["description"].values())[0]}")
