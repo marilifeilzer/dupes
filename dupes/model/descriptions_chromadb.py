4815a6fcb8de5b97d393b6ed97bb868b5e9bfe86
Hannes Geldof <hannes.geldof@gmail.com>
2025-12-05 12:04:38 +0100

    feature: description embeddings

--- dupes/model/descriptions_chromadb.py
+++ dupes/model/descriptions_chromadb.py
@@ -2,11 +2,14 @@ import pandas as pd
 from sentence_transformers import SentenceTransformer
 import chromadb
 
+# Load raw data
+df = pd.read_csv('/Users/panamas/code/marili/dupes/raw_data/products_data.csv')
+
+# Common instances
 model = SentenceTransformer("all-mpnet-base-v2")
 chroma_client = chromadb.PersistentClient(path="raw_data/")
 
-# df = pd.read_csv('/Users/panamas/code/marili/dupes/raw_data/products_data.csv')
-
+# Helper functions
 def embedding_description_get_data(df: pd.DataFrame):
     dropped = df[['product_id', 'description']]
     dropped = df.dropna(subset=['description'], axis=0)
@@ -24,28 +27,17 @@ def embedding_description_populate_chromadb(dropped: pd.DataFrame, embeddings):
     )
     return collection
 
-def embedding_description_query_chromadb(collection, query, n_results = 5):
+def embedding_description_query_chromadb(collection, query, n_results):
     query_embedding = model.encode(query, show_progress_bar=False)
     results = collection.query(
         query_embeddings=[query_embedding],
         n_results=n_results)
-
-
     return results
 
-# get_data
-# embed
-# populate_chromadb
-# query_chromadb
-
-if __name__ == "__main__":
-    df = pd.read_csv('/Users/panamas/code/marili/dupes/raw_data/products_data.csv')
-    dropped = embedding_description_get_data(df)
-    embeddings = embedding_description_embed(dropped)
-    collection = embedding_description_populate_chromadb(dropped, embeddings)
-
-    #collection = chroma_client.get_or_create_collection(name="description_embed")
-
-    breakpoint()
-    result = embedding_description_query_chromadb(collection, 'Im looking for greasy and curly hair.'
-)
+# Main functionality
+def embedding_description_get_recommendation(query, n_results = 5):
+    dropped_desc = embedding_description_get_data(df)
+    embeddings_desc = embedding_description_embed(dropped_desc)
+    collection_desc = embedding_description_populate_chromadb(dropped_desc, embeddings_desc)
+    recommendations_desc = embedding_description_query_chromadb(collection_desc, query, n_results)
+    return recommendations_desc
