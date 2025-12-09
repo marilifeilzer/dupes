7df17136a8299312d9ec3f7a9816ed20ddfb524d
Hannes Geldof <hannes.geldof@gmail.com>
2025-12-09 17:19:02 +0100

    price prediction change: not ready

--- dupes/api/fast.py
+++ dupes/api/fast.py
@@ -12,9 +12,7 @@ app.state.model = load_model()
 
 @app.get("/predict_price")
 def get_price_prediction(volume_ml: int  = 350.0,
-                         propiedad: str = 'Detergente, Anti-rotura capilar, Nutritivo, Protector, Reparador, Refrescante, Espuma',
-                         ingredients_raw: str = "Water, Cetearyl Alcohol, PPG-3 Benzyl Ether Myristate, Caprylic/Capric Triglyceride, Cetyl Alcohol,Octyldodecyl Ricinoleate, Quaternium-91, Cetrimonium Chloride, Divinyldimethicone/Dimethicone Copolymer, Behentrimonium Chloride, Glycerin, Cetyl Esters, Isododecane, Bis-Aminopropyl Diglycol Dimaleate, Fragrance, Panthenol, Phospholipids, Dimethicone PEG-7 Isostearate, Pseudozyma Epicola/Argania Spinosa Kernel Oil Ferment Filtrate, Pseudozyma Epicola/Camellia Sinensis Seed Oil Ferment Extract Filtrate, Tocopheryl Linoleate/Oleate, Quaternium-95, Propanediol, Punica Granatum Extract, Morinda Citrifolia Fruit Extract, PEG-8, Euterpe Oleracea Fruit Extract, Camellia Sinensis Seed Oil, Crambe Abyssinica Seed Oil, Hydroxypropyl Cyclodextrin, Persea Gratissima (Avocado) Oil, Vitis Vinifera (Grape) Seed Oil, Disodium EDTA, Polysilicone-15, C11-15 Pareth-7, Hydroxypropyl Guar, Glycine Soja (Soybean) Oil, PEG-45M, PEG-7 Amodimethicone, Amodimethicone, C12-13 Pareth-23, C12-13 Pareth-3, Laureth-9, Pentaerythrityl Tetra-Di-T-Butyl Hydroxyhydrocinnamate, PEG-4, Phenoxyethanol, Hexyl Cinnamal.",
-                         manufacturer_name: str = 'Obelis S.A'
+                         ingredients_raw: str = "Water, Cetearyl Alcohol, PPG-3 Benzyl Ether Myristate, Caprylic/Capric Triglyceride, Cetyl Alcohol,Octyldodecyl Ricinoleate, Quaternium-91, Cetrimonium Chloride, Divinyldimethicone/Dimethicone Copolymer, Behentrimonium Chloride, Glycerin, Cetyl Esters, Isododecane, Bis-Aminopropyl Diglycol Dimaleate, Fragrance, Panthenol, Phospholipids, Dimethicone PEG-7 Isostearate, Pseudozyma Epicola/Argania Spinosa Kernel Oil Ferment Filtrate, Pseudozyma Epicola/Camellia Sinensis Seed Oil Ferment Extract Filtrate, Tocopheryl Linoleate/Oleate, Quaternium-95, Propanediol, Punica Granatum Extract, Morinda Citrifolia Fruit Extract, PEG-8, Euterpe Oleracea Fruit Extract, Camellia Sinensis Seed Oil, Crambe Abyssinica Seed Oil, Hydroxypropyl Cyclodextrin, Persea Gratissima (Avocado) Oil, Vitis Vinifera (Grape) Seed Oil, Disodium EDTA, Polysilicone-15, C11-15 Pareth-7, Hydroxypropyl Guar, Glycine Soja (Soybean) Oil, PEG-45M, PEG-7 Amodimethicone, Amodimethicone, C12-13 Pareth-23, C12-13 Pareth-3, Laureth-9, Pentaerythrityl Tetra-Di-T-Butyl Hydroxyhydrocinnamate, PEG-4, Phenoxyethanol, Hexyl Cinnamal"
                          ):
 
     # Create dataframe with the input variables for the prediction
@@ -27,8 +25,6 @@ def get_price_prediction(volume_ml: int  = 350.0,
     model = app.state.model
 
     # Make the prediction
-
-
     pred_price_ml = model.predict(preproc).tolist()
     pred_price = pred_price_ml[0] * volume_ml
 
@@ -38,65 +34,62 @@ def get_price_prediction(volume_ml: int  = 350.0,
 # embedding_description_get_recommendation()
 # df = pd.read_csv("/Users/lewagon/code/marilifeilzer/dupes/raw_data/products_data__0412.csv")
 
-@app.get("/")
-def index():
-    return {"working": True}
+# @app.get("/")
+# def index():
+#     return {"working": True}
 
-@app.get("/recommend")
-def get_recommendation(description: str):
+# @app.get("/recommend")
+# def get_recommendation(description: str):
 
-    recommendation = embedding_description_query_chromadb(description)
+#     recommendation = embedding_description_query_chromadb(description)
 
-    return recommendation
+#     return recommendation
 
+# df_cleaned= pd.read_csv('/home/panamas/code/marili/dupes/raw_data/products_cleaned.csv')
+# dropped =  df_cleaned.dropna(subset=["formula"], axis=0)
 
+# @app.get("/recommend_ingredients")
+# def get_recommendation_ingredients(
+#     # product_id: str,
+#     formula: str = "H2O', 'C10H14N2Na2O8', 'C19H38N2O3', 'PPG-5-Ceteth-20', 'C41H80O17', 'C7H5NaO2', 'C8H10O2', 'C6H8O7', 'C16H32O6', 'C10H18O', 'Na4EDTA', 'C9H6O2', 'C10H16', 'C10H20O', 'polyquaternium-7', 'C29H50O2'",
+#     color_de_cabello: str = "todos_los_colores_de_cabello",
+#     tipo_de_cabello: str = "Todo tipo de cabello",
+#     propiedad: str = "Detergente" ,
+# ):
 
+#     product = pd.DataFrame({
+#         # "product_id": [product_id],
+#         "formula": [formula],
+#         "color_de_cabello": [color_de_cabello],
+#         "tipo_de_cabello": [tipo_de_cabello],
+#         "propiedad": [propiedad]
+#     })
 
-df_cleaned= pd.read_csv('/home/marili/code/marilifeilzer/dupes/raw_data/products_cleaned.csv')
-dropped =  df_cleaned.dropna(subset=["formula"], axis=0)
+#     cols = ['formula', 'color_de_cabello', 'tipo_de_cabello', 'propiedad']
 
-@app.get("/recommend_ingredients")
-def get_recommendation_ingredients(
-    # product_id: str,
-    formula: str = "H2O', 'C10H14N2Na2O8', 'C19H38N2O3', 'PPG-5-Ceteth-20', 'C41H80O17', 'C7H5NaO2', 'C8H10O2', 'C6H8O7', 'C16H32O6', 'C10H18O', 'Na4EDTA', 'C9H6O2', 'C10H16', 'C10H20O', 'polyquaternium-7', 'C29H50O2'",
-    color_de_cabello: str = "todos_los_colores_de_cabello",
-    tipo_de_cabello: str = "Todo tipo de cabello",
-    propiedad: str = "Detergente" ,
-):
+#     for col in cols:
+#         product[col] = product[col].apply(
+#             lambda x: x if isinstance(x, list) else x.split(',')
+#     )
 
-    product = pd.DataFrame({
-        # "product_id": [product_id],
-        "formula": [formula],
-        "color_de_cabello": [color_de_cabello],
-        "tipo_de_cabello": [tipo_de_cabello],
-        "propiedad": [propiedad]
-    })
+#     results = main_results(product)
+#     product_ids= results['ids'][0]
 
-    cols = ['formula', 'color_de_cabello', 'tipo_de_cabello', 'propiedad']
+#     product_names = [df_cleaned.
+#                      loc[df_cleaned["product_id"]==product, ["product_name","price_eur", "description"]]for product in product_ids]
 
-    for col in cols:
-        product[col] = product[col].apply(
-            lambda x: x if isinstance(x, list) else x.split(',')
-    )
-
-    results = main_results(product)
-    product_ids= results['ids'][0]
-
-    product_names = [df_cleaned.
-                     loc[df_cleaned["product_id"]==product, ["product_name","price_eur", "description"]]for product in product_ids]
-
-    return product_names
+#     return product_names
 
 
-@app.get("/recommend_dupe")
-def get_recommendation_ingredients(
-    product_id: str
-):
+# @app.get("/recommend_dupe")
+# def get_recommendation_ingredients(
+#     product_id: str
+# ):
 
-    results= main_res_product_id(product_id, dropped)
+#     results= main_res_product_id(product_id, dropped)
 
-    product_ids= results['ids'][0]
+#     product_ids= results['ids'][0]
 
-    df = dropped.loc[dropped["product_id"].isin(product_ids), ["product_name","price_eur", "description"]]
+#     df = dropped.loc[dropped["product_id"].isin(product_ids), ["product_name","price_eur", "description"]]
 
-    return {"prodcut_names":df.fillna("No data").to_dict(orient="records")}
+#     return {"prodcut_names":df.fillna("No data").to_dict(orient="records")}
