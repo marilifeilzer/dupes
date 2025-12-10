2fd2a6ee1dbfd91504da87d7a2f7f6eb97849ed8
Marili Feilzer <marilifeilzer@gmail.com>
2025-12-10 10:38:33 +0100

    feat: interface layout

--- dupes/interface/interface.py
+++ dupes/interface/interface.py
@@ -4,9 +4,9 @@ from streamlit_card import card
 
 pages = {
     "HOME":
-        [st.Page("interface.py", title="Go to homepage")],
+        [st.Page("home.py", title="Go to homepage")],
     "DUPE FINDER":
-        [st.Page("shampoo_ui.py", title="Find your dupe")],
+        [st.Page("shampoo.py", title="Find your dupe")],
     "RECOMMENDATION": [
       st.Page("product_description.py", title="Find your recommendation")
      ]
@@ -14,11 +14,3 @@ pages = {
 
 pg = st.navigation(pages, position="top")
 pg.run()
-
-st.markdown("""
-            # Welcome to Dupes! 👋🏻👋🏻
-
-            ## Our mission is simple: help you find the right shampoo for your needs at a price you’ll love
-
-            """)
-
