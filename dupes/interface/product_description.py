51d00432eca9e1c1da8f6d44e612a6c9086908d8
Marili Feilzer <marilifeilzer@gmail.com>
2025-12-12 14:32:58 +0100

    interface cleanup

--- dupes/interface/product_description.py
+++ dupes/interface/product_description.py
@@ -10,7 +10,7 @@ with st.container():
     st.title('PRODUCT DESCRIPTION')
     st.caption("""## Let us know what you're looking for in your new shampoo and we will find affordable options.""", width="stretch", text_alignment="left")
 
-    nlp_text = st.text_input('### Type in the cell below your description.', placeholder="frizz control, hydration, volume, repair?")
+    nlp_text = st.text_input('### Type in the cell below your description.', placeholder="frizz control, hydration, volume, repair?", label_visibility='collapsed')
 
 
 
@@ -37,21 +37,19 @@ if nlp_text:
                 st.image(f"img/images/{prediction["product_id"]}.jpg")
                 st.title(f"{prediction['product_name']}")
                 st.caption(f"{prediction['en_description']}")
-                st.caption(f"This shampoo sells for: €{prediction['price_eur']} for {prediction['volume_ml']} ml,")
-                st.caption(f"But its fair value is around: € {round(prediction['price_prediction'],2)} for {prediction['volume_ml']} ml.")
-                st.text(f"Thats €{round(prediction['price_prediction'] - prediction['price_eur'],2)} in savings")
-                st.text(f"Dupe")
+                st.caption(f"## This shampoo sells for: €**{prediction['price_eur']}** for {prediction['volume_ml']} ml,")
+                st.caption(f"## But its fair value is around: €**{round(prediction['price_prediction'],2)}** for {prediction['volume_ml']} ml.")
+                st.caption(f"# This is a undervalued product.")
 
         else:
 
             with st.container(border= True):
                 st.image(f"img/images/{prediction["product_id"]}.jpg")
                 st.title(f"{prediction['product_name']}")
-                # st.caption(f"{list(predict_prices["prediction"][0]['product_name']}")
                 st.caption(f"{prediction['en_description']}")
-                st.caption(f"The product is priced at: €{prediction['price_eur']} for {prediction['volume_ml']} ml in stores")
-                st.caption(f"Based on our analysis, a fair price would be € {round(prediction['price_prediction'],2)} for {prediction['volume_ml']} ml.")
-                st.text(f"Not dupe")
+                st.caption(f"The product is priced at: €**{prediction['price_eur']}** for {prediction['volume_ml']} ml in stores")
+                st.caption(f"Based on our analysis, a fair price would be €**{round(prediction['price_prediction'],2)}** for {prediction['volume_ml']} ml.")
+                st.caption(f"# This is a overvalued product.")
 
 
 
