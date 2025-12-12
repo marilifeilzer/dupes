e508dc881ec413d9d56dce77eaceea1a5c6039cf
Jonathan Moreno <jonathanmorenoram@gmail.com>
2025-12-12 14:50:19 +0100

    fix: bold

--- dupes/interface/product_description.py
+++ dupes/interface/product_description.py
@@ -8,7 +8,7 @@ import streamlit as st
 with st.container():
 
     st.title('PRODUCT DESCRIPTION')
-    st.write("""## Let us know what you're looking for in your new shampoo and we will find affordable options.""", width="stretch", text_alignment="left")
+    st.caption("""## Let us know what you're looking for in your new shampoo and we will find affordable options.""", width="stretch", text_alignment="left")
 
     nlp_text = st.text_input('### Type in the cell below your description.', placeholder="frizz control, hydration, volume, repair?", label_visibility='collapsed')
 
@@ -36,20 +36,20 @@ if nlp_text:
             with st.container(border= True):
                 st.image(f"img/images/{prediction["product_id"]}.jpg")
                 st.title(f"{prediction['product_name']}")
-                st.write(f"## {prediction['en_description']}")
-                st.write(f"## This shampoo sells for €**{prediction['price_eur']}** for {prediction['volume_ml']} ml.")
-                st.write(f"## Based on our analysis, a fair price would be €**{round(prediction['price_prediction'],2)}** for {prediction['volume_ml']} ml.")
-                st.write(f"# This is an **undervalued** product.")
+                st.caption(f"## {prediction['en_description']}")
+                st.caption(f"## This shampoo sells for €**{prediction['price_eur']}** for {prediction['volume_ml']} ml.")
+                st.caption(f"## Based on our analysis, a fair price would be €**{round(prediction['price_prediction'],2)}** for {prediction['volume_ml']} ml.")
+                st.caption(f"# This is an **undervalued** product.")
 
         else:
 
             with st.container(border= True):
                 st.image(f"img/images/{prediction["product_id"]}.jpg")
                 st.title(f"{prediction['product_name']}")
-                st.write(f"## {prediction['en_description']}")
-                st.write(f"## This shampoo sells for €**{prediction['price_eur']}** for {prediction['volume_ml']} ml.")
-                st.write(f"## Based on our analysis, a fair price would be €**{round(prediction['price_prediction'],2)}** for {prediction['volume_ml']} ml.")
-                st.write(f"# This is a **overvalued** product.")
+                st.caption(f"## {prediction['en_description']}")
+                st.caption(f"## This shampoo sells for €**{prediction['price_eur']}** for {prediction['volume_ml']} ml.")
+                st.caption(f"## Based on our analysis, a fair price would be €**{round(prediction['price_prediction'],2)}** for {prediction['volume_ml']} ml.")
+                st.caption(f"# This is a **overvalued** product.")
 
 
 
